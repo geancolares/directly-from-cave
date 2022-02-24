22d84a68fc1a81716d3123ac030329913868821d
geancolares <60328679+geancolares@users.noreply.github.com>
2022-02-24 07:53:21 -0300

    3d projection was modified

--- _3dprojection_from_scratch/main.js
+++ _3dprojection_from_scratch/main.js
@@ -1,11 +1,10 @@
-var points = []
-var visiblePoints = []
-var pointIntensities = []
-var pointRgbs = []
+var points = visiblePoints = pointIntensities = pointRgbs = []
 
 const Z_VECTOR = [0,0,-1]
-var lightPoint = [70,-50,-150]
+var lightPoint = [200,-200,-2000]
 
+var blockSize = 2
+var xPos = yPos = 0
 var  offsetx = 100, offsety = 100
 
 function produtscl(v1, v2){
@@ -19,12 +18,12 @@ function precise(number,decimals){
 function coordinates(pixelDensity){
     let radius = 100
     let points = []
+    let z
     for(let x=-radius; x<=radius; x+=pixelDensity){
         for(let y=-radius; y<=radius; y+=pixelDensity){
-            let z = - Math.sqrt(radius**2 - x**2 - y **2)
+            z = -Math.sqrt(radius**2 -x**2 -y**2)
             z = precise(z,5)
             if(!isNaN(z)){
-                console.log(x,y,z)
                 points.push([x,y,z])
             }
         }
@@ -38,18 +37,12 @@ function pointIntensity(lightPosition,point){
 }
 
 function pointRgb(intensity,minIntensity,maxIntensity){
-    let rgb = ((intensity-minIntensity)*250)/(maxIntensity-minIntensity)
+    let rgb = ((intensity-minIntensity)*255)/(maxIntensity-minIntensity)
     return rgb
 }
 
-function init(){
-    points = coordinates(2)
-    console.log(points)
-    points.forEach(x=>{
-        if(produtscl(Z_VECTOR,x)>0){
-            visiblePoints.push([x[0],x[1],x[2]])
-        }
-    })
+function setPointVectors(){
+    pointIntensities = []
     visiblePoints.forEach(x=>{
         pointIntensities.push(pointIntensity(lightPoint,x))
     })
@@ -57,29 +50,36 @@ function init(){
     let minIntensity = Math.min(...pointIntensities)
     let maxIntensity = Math.max(...pointIntensities)
 
+    pointRgbs = []
     pointIntensities.forEach(x=>{
         pointRgbs.push(pointRgb(x,minIntensity,maxIntensity))
     })
 }
 
+function init(){
+    points = coordinates(blockSize)
+
+    points.forEach(x=>{
+        if(produtscl(Z_VECTOR,x)>0){
+            visiblePoints.push(x)
+        }
+    })
+
+    setPointVectors()
+}
+
 function update(){
+    ctx.clearRect(0,0,cnv.width,cnv.height)
+
     for(let i=0; i<visiblePoints.length; i++){
-        ctx.fillStyle = 'rgb('+pointRgbs[i]+100+','+0+','+0+')'
-        ctx.fillRect(visiblePoints[i][0]+offsetx,visiblePoints[i][1]+offsety,2,2)
+        ctx.fillStyle = 'rgb('+0+','+pointRgbs[i]+','+0+')'
+        ctx.fillRect(xPos+visiblePoints[i][0]+offsetx,yPos+visiblePoints[i][1]+offsety,blockSize,blockSize)
     }
 
-    pointIntensities = []
-    visiblePoints.forEach(x=>{
-        pointIntensities.push(pointIntensity(lightPoint,x))
-    })
+    ctx.fillStyle = 'rgb('+255+','+0+','+255+')'
+    ctx.fillRect(offsetx+lightPoint[0]-2,offsety+lightPoint[1]-2,4,4)
 
-    minIntensity = Math.min(...pointIntensities)
-    maxIntensity = Math.max(...pointIntensities)
-
-    pointRgbs = []
-    pointIntensities.forEach(x=>{
-        pointRgbs.push(pointRgb(x,minIntensity,maxIntensity))
-    })
+    setPointVectors()
 }
 
 init()
--- _3dprojection_from_scratch/treatEvents.js
+++ _3dprojection_from_scratch/treatEvents.js
@@ -1,4 +1,30 @@
+function moviment(event){
+    switch(event.key.toLowerCase()){
+        case 'arrowdown':{
+            yPos+=2
+            lightPoint[1]-=2
+            break
+        }
+        case 'arrowup':{
+            yPos-=2
+            lightPoint[1]+=2
+            break
+        }
+        case 'arrowleft':{
+            xPos-=2
+            lightPoint[0]+=2
+            break
+        }
+        case 'arrowright':{
+            xPos+=2
+            lightPoint[0]-=2
+            break
+        }
+    }
+}
+
 cnv.addEventListener('mousemove',function(event){
-    
-    lightPoint = [event.layerX-100,event.layerY-100,-150]
-})
+    console.log(Math.random())
+    lightPoint = [event.layerX-100-xPos,event.layerY-100-yPos,-150]})
+
+document.addEventListener('keydown',moviment)
